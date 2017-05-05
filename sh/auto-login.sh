296a1a27f04522302af73203df3a24eecab96404
MR_admin <fdsys@nfschina.com>
2017-05-05 17:55:36 +0800

    添加注释

--- sh/auto-login.sh
+++ sh/auto-login.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/expect
+filename:auto-login.sh
+func:自动输入密码，需要装export工具(替换 用户名、密码、IP，个性化操作)
+usage:
+
 set timeout 30
 spawn ssh -l zhiyuan 192.168.7.171
 expect "password:"
 send "123\r"
+
 expect "zhiyuan"
 send "cd /mnt/isoRoom/zhiyuan\r"
 interact 
